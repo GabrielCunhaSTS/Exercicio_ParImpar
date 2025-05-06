c1d7cb85bf321f1dea5ac2be2bb52d39576915fb
Gabriel <ggabrielcunha06@gmail.com>
2025-05-06 16:19:57 -0300

    Exercicio para descobrir se o numero digitado é par ou impar finalizado!

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int numero;
+Console.WriteLine("------ Bem-vindo(a) ao programa Par & Impar ------");
+
+Console.Write("Digite um número para conferir se ele é par ou impar: ");
+
+while (!int.TryParse(Console.ReadLine(), out numero) || numero < 0)
+{
+    Console.Write("Digite um número válido. Digite novamente: ");
+}
+
+if (numero % 2 == 0){
+    Console.WriteLine($"O número que você digitou é {numero} e é par");
+}else{
+    Console.WriteLine($"O número que você digitou é {numero} e é impar");
+}
+Console.Write("Digite qualquer tecla para finalizar o programa!");
+Console.ReadKey();
+Console.Clear();
